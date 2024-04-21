3be89d086fcc5bd1b63359274615194660a82e09
TafadzwaG <tafadzwagashirah@gmail.com>
2024-04-21 13:33:45 +0200

    added the list of all cancellations when you click the Cancellation Link

---
+++ biVerifier/Controllers/CancellationsController.cs
@@ -0,0 +1,45 @@
+﻿using biVerifier.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.OleDb;
+
+namespace biVerifier.Controllers
+{
+    public class CancellationsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=E:\CODING_HASHIRA\PROJECTS\.NET\databaseAccess\VERIFIER1.accdb;Persist Security Info=False;";
+
+            string query = "SELECT * FROM Cancellations";
+
+            var cancellationsDataList = new List<CancellationsData>();
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            using (OleDbCommand command = new OleDbCommand(query, connection))
+
+                try
+                {
+                    connection.Open();
+                    using (OleDbDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var cData = new CancellationsData();
+                            cData.ID = (int)reader["ID"];
+                            cData.Client = reader["Client"].ToString();
+                            cData.Status = reader["Status"].ToString();
+                            cData.Cancel_Billing = reader["Cancel_Billing"].ToString();
+                            cancellationsDataList.Add(cData);
+                        }
+                    }
+                }
+                catch (OleDbException ex)
+                {
+                    // Handle exception
+                }
+
+            return View(cancellationsDataList);
+        }
+    }
+}
---
+++ biVerifier/Models/CancellationsData.cs
@@ -0,0 +1,30 @@
+﻿namespace biVerifier.Models
+{
+    public class CancellationsData
+    {
+        public int ID { get; set; }
+        public string? Client { get; set; }
+        public string? SageAcc { get; set; }
+        public string? Site { get; set; }
+        public string? Contact_Person { get; set; }
+        public string? Account_Manager { get; set; }
+        public string? Cancellation_Month { get; set; }
+        public string? Cancellation_End_Date { get; set; }
+
+        public string? Reason { get;  set; }
+        public string? Notes { get;  set; }
+        public string? Reduced_Value_Ex_Vat { get;  set; }
+        public string? TechResponsible { get; set; }
+
+        public string? Total_Channels { get; set; }
+        public string? Platform { get; set; }
+        public string? Cancel_GSM { get; set; }
+        public string? Cancel_DNS { get; set; }
+        public string? Cancel_LPR_Licenses { get; set; }
+        public string? Cancel_Video_Analytics_Licenses { get; set; }
+        public string? Cancel_Internet_Connectivity { get; set; }
+        public string? Cancel_Billing { get; set; }
+        public string? Status { get; set; }
+
+    }
+}
