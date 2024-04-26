89f3d1ad6b4f97a4cbc86c5a84460eecac8891e6
TafadzwaG <tafadzwagashirah@gmail.com>
2024-04-26 12:11:25 +0200

    Technical Cancel Client Type

--- biVerifier/Controllers/ReportsController.cs
+++ biVerifier/Controllers/ReportsController.cs
@@ -598,9 +598,71 @@ namespace biVerifier.Controllers
         //Technical Reports 
         public IActionResult TechnicalByClientType(string client)
         {
-            return View();
+
+            if (client == null)
+            {
+                return View(new List<TechCancelData>());
+            }
+
+            string connectionString = @"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=E:\CODING_HASHIRA\PROJECTS\.NET\databaseAccess\VERIFIER1.accdb;Persist Security Info=False;";
+
+            string query;
+            var techCancelDataList = new List<TechCancelData>();
+
+            // Check if client is "other" to adjust the query and parameters
+            if (client.ToLower() == "other")
+            {
+                query = "SELECT * FROM TechCancel WHERE Platform IS NULL OR Platform = '' OR Platform = 'N/A'";
+            }
+            else
+            {
+                query = "SELECT * FROM TechCancel WHERE Platform = ?";
+            }
+
+            using (OdbcConnection connection = new OdbcConnection(connectionString))
+            using (OdbcCommand command = new OdbcCommand(query, connection))
+            {
+                // Add parameter for the client if it's not "other"
+                if (client.ToLower() != "other")
+                {
+                    command.Parameters.AddWithValue("@Platform", client);
+                }
+
+                try
+                {
+                    connection.Open();
+                    using (OdbcDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var cData = new TechCancelData();
+                            cData.ID = reader["ID"] != DBNull.Value ? Convert.ToInt32(reader["ID"]) : null;
+                            cData.ClientID = reader["ClientID"].ToString();
+                            cData.SiteID = reader["SiteID"].ToString();
+                            cData.Date = reader["Date"].ToString();
+                            cData.TechResponsible = reader["TechResponsible"].ToString();
+                            cData.Total_Channels = reader["Total_channels"] != DBNull.Value ? Convert.ToInt32(reader["Total_channels"]) : null;
+                            cData.Platform = reader["Platform"].ToString();
+                            cData.Cancel_GSM = reader["Cancel_GSM"].ToString();
+                            cData.Cancel_DNS = reader["Cancel_DNS"].ToString();
+                            cData.Cancel_LPR_Licenses = reader["Cancel_LPR_Licenses"].ToString();
+                            cData.Cancel_Video_Analytics_Licenses = reader["Cancel_Internet_Connectivity"].ToString();
+                            cData.Cancel_Billing = reader["Cancel_Billing"].ToString();
+                            techCancelDataList.Add(cData);
+                        }
+                    }
+                }
+                catch (OdbcException ex)
+                {
+                    // Handle exception
+                    Console.WriteLine("Error whilst fetching Data: " + ex.Message);
+                }
+            }
+            Console.WriteLine("Paltform Search Count" + " " + techCancelDataList);
+            return View(techCancelDataList);
         }
-        
+
+
         public IActionResult TechnicalByTechnician(string technician)
         {
             return View();
