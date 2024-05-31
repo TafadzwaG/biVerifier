e9a326dfb50052e4e27f3c053cddaca9898b81df
Tafadzwa Gashira <tafadzwagashirah@gmail.com>
2024-05-31 12:27:40 +0200

    Added a search query

--- biVerifier/Controllers/CrmController.cs
+++ biVerifier/Controllers/CrmController.cs
@@ -75,6 +75,78 @@ namespace biVerifier.Controllers
 
             return View(crmDataList);
         }
+
+        public IActionResult SearchIndex(string searchString)
+        {
+            string connectionString = @"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=E:\CODING_HASHIRA\PROJECTS\.NET\databaseAccess\VERIFIER2.accdb;Persist Security Info=False;";
+            string query = "SELECT * FROM CRM";
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                query += " WHERE Client LIKE ? OR Contact_Person LIKE ? OR Email LIKE ?";
+            }
+
+            var crmDataList = new List<Crm>();
+
+            using (OdbcConnection connection = new OdbcConnection(connectionString))
+            using (OdbcCommand command = new OdbcCommand(query, connection))
+            {
+                if (!string.IsNullOrEmpty(searchString))
+                {
+                    command.Parameters.AddWithValue("Client", "%" + searchString + "%");
+                    command.Parameters.AddWithValue("Contact_Person", "%" + searchString + "%");
+                    command.Parameters.AddWithValue("Email", "%" + searchString + "%");
+                }
+
+                try
+                {
+                    connection.Open();
+                    using (OdbcDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var crmData = new Crm
+                            {
+                                ID = (int)reader["ID"],
+                                Client = reader["Client"].ToString(),
+                                Contact_Person = reader["Contact_Person"].ToString(),
+                                Email = reader["Email"].ToString(),
+                                Contact_Number = reader["Contact_Number"].ToString(),
+                                No = reader["No"].ToString(),
+                                Street = reader["Street"].ToString(),
+                                Suburb = reader["Suburb"].ToString(),
+                                City = reader["City"].ToString(),
+                                Province = reader["Province"].ToString(),
+                                LeadSource = reader["LeadSource"].ToString(),
+                                Service = reader["Service"].ToString(),
+                                Market = reader["Market"].ToString(),
+                                Category = reader["Category"].ToString(),
+                                Consultant = reader["Consultant"].ToString(),
+                                Branch = reader["Branch"].ToString(),
+                                Status = reader["Status"].ToString(),
+                                Probability = reader["Probability"].ToString(),
+                                leadyear = reader["leadyear"].ToString(),
+                                leadmonth = reader["leadmonth"].ToString(),
+                                liveyear = reader["liveyear"].ToString(),
+                                livemonth = reader["livemonth"].ToString(),
+                                TotalMonitoringFees = reader["TotalMonitoringFees"].ToString(),
+                                Onceoffsetupcosts = reader["Onceoffsetupcosts"].ToString(),
+                                Install_Comm = reader["Install_Comm"].ToString(),
+                                ManagedServicesFees = reader["ManagedServicesFees"].ToString()
+                            };
+                            crmDataList.Add(crmData);
+                        }
+                    }
+                }
+                catch (OdbcException ex)
+                {
+                    Console.WriteLine("There was an error " + ex.Message);
+                }
+            }
+
+            return View("Index", crmDataList);
+        }
+
         public IActionResult RetrieveCrmData()
         {
 
